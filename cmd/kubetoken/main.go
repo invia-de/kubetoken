d5867a081f912c5b3b9e6fbcd9b72543b61d905c
Joerg Schueppel <schorsus@gmx.de>
2019-03-13 11:36:08 +0100

    Show Version

--- cmd/kubetoken/main.go
+++ cmd/kubetoken/main.go
@@ -336,7 +336,7 @@ func processCertificateResponse(kubeconfig string, result *kubetoken.Certificate
 }
 
 func compareVersionsAndExit(host string, certcheck bool) {
-	fmt.Printf("Kubetoken Client Version %s", kubetoken.Version)
+	fmt.Printf("Kubetoken Client Version %s \n", kubetoken.Version)
 	versionURL := host + "/version"
 	httpClientWithSelfSignedTLS := getTransport(certcheck)
 	client := &http.Client{Transport: httpClientWithSelfSignedTLS}
